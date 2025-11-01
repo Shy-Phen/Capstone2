5eddf087d92bd1c9cbdec52dc17cd0064f6026ab
Phen <rdcoca88@gmail.com>
2025-11-01 12:47:25 +0800

    remove ratelimiter

--- backend/src/routes/geminApiRoute.js
+++ backend/src/routes/geminApiRoute.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import { gemini } from "../contollers/geminiApiController.js";
-import { rateLimit } from "../middleware/ratelLimiting.js";
 
 const route = express.Router();
 
-route.post("/", verifyToken, rateLimit, gemini);
+route.post("/", verifyToken, gemini);
 
 export default route;
